dccee5c5c868f307952e1e64a6841ac934d22fa3
Wallace Silva <wallace.silva@questmanager.com>
2019-03-22 13:52:45 -0300

    inserção de algumas rotas e modelos

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const app = express();
 const dbConfig = require('./config/config')
 
 mongoose.Promise = global.Promise;
+
+//Conexão com o banco
 mongoose.connect(dbConfig.dbStringConexao, {useNewUrlParser:true})
+
+//Middleware com as rotas
+const login = require('./routes/loginRoutes')
+
+app.use('/api',login)
 
 app.listen(3000, () =>{
     console.log('running on port 3000')
