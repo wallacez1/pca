d8ea520e577ca7a1288bde28fdcf7aa212c5aa3d
Wallace Silva <wallace.silva@questmanager.com>
2019-03-22 14:20:19 -0300

    atualização readme

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+Requerimentos: Ter node.js instalado
+
+***passos para rodar o back end***
+
+1 - Baixar repositório
+
+2 - entrar na pasta do repositório pelo terminal
+
+3 - executar o comando npm install, para baixar as dependências
+
+4 - npm start para executar
--- models/servico.js
+++ models/servico.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
 const servicoSchema = mongoose.Schema({
-   
+    nome: {type:String},
+    valor: {type:Number,default:0},
+    dataCadastro: {type: Date, default: DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss.fff")}
 });
 
 module.exports =  mongoose.model('usuario',servicoSchema)
