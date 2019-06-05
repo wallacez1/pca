0bdcd455665e9b026629d858a7bb9f4a33a59b1f
LeandroPA <leandroalencar@unigranrio.br>
2019-06-05 00:06:12 -0300

    Concluindo endpoint de retorno de like e acao de like

--- controllers/qualifyController.js
+++ controllers/qualifyController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const produtoModel = require('../models/produto');
 const qualificacaoSchema = require('../models/qualificacao');
 const userModel = require('../models/user');
-const mongoose = require('mongoose');
 const qualificacaoModel = mongoose.model('qualificacoes', qualificacaoSchema);
 
 const ACAO_LIKE = 1;
@@ -56,7 +55,7 @@ module.exports = {
                 return;
             }
             produto = produto.toObject();
-
+            
             //Caso um produto não possua nenhuma qualificacao,
             //entao nao vai existir um documento para o mesmo
             //Entao, vamos adiantar a resposta que nao possui
@@ -74,61 +73,23 @@ module.exports = {
 
                 qualificacaoModel.aggregate([
                     {$match: {_id: produto.qualificacoes}},
-                    {$unwind: "$acoes"},
-                    {$match: {_id: user._id}},
-                    {$group: {_id: "$acoes.acao", count: {$sum: 1}}}
+                    {$unwind: '$acoes'},
+                    {$project: {user_id: '$acoes.user', user_acao: '$acoes.acao'}},
+                    {$match: {"$acoes.user": user._id}},
+                    //{$match: {user_id: 'LeandroA'}},
+                    {$group: {_id: "$user_acao"}}
                 ], (err, array) => {
-                    let retorno = {likes:0, deslikes:0};
-                    for (index in array) {
-    
-                        let element = array[index];
-                        //LIKES
-                        if (element._id == 1) {
-                            retorno.likes = element.count;
-                        //DESLIKES
-                        } else if (element._id == 0) {
-                            retorno.deslikes = element.count;
-                        }
+                    const {_id} = !array && array.length == 1 ? array[0] : {}; 
+                    const resultado = {resultado: ACAO_NENHUMA};
+                    if (_id == ACAO_LIKE || _id == ACAO_DESLIKE) {
+                        resultado.resultado = _id;
                     }
-                    res.status(200).json(retorno);
+                    res.status(200).json(resultado);
                 });
-                qualificacaoModel.findById(produto.qualificacoes, (err, qualificacao) => {
-                    if (err) {
-                        res.status(500).json({message: "Qualificação não encontrada: " + err});
-                        return;
-                    }
-
-
-                });
-
-            });
-
-            // console.log ("Produto: " + produto);
-            // console.log ("Produto: " + produto._id);
-            // console.log ("Produto: " + produto.qualificacoes);
-            // console.log ("Produto: " + produto.isOnline);
-            qualificacaoModel.aggregate([
-                {$match: {_id: produto.toObject().qualificacoes}},
-                { $unwind : "$acoes" },
-                {$group: {_id: "$acoes.acao", count: {$sum: 1}}}
-            ], (err, array) => {
-                let retorno = {likes:0, deslikes:0};
-                for (index in array) {
-
-                    let element = array[index];
-                    //LIKES
-                    if (element._id == ACAO_LIKE) {
-                        retorno.likes = element.count;
-                    //DESLIKES
-                    } else if (element._id == ACAO_DESLIKE) {
-                        retorno.deslikes = element.count;
-                    }
-                }
-                res.status(200).json(retorno);
             });
         });
-
     },
+
     action(req, res) {
         if (!req.query.produto) {
             res.status(400).json({message: "Id do produto não informado"});
@@ -139,30 +100,30 @@ module.exports = {
             res.status(400).json({message: "Ação não informada"});
             return;
         }
-        // qualificacaoModel.create({acoes:[]}).then(qual => {
-        //              console.log("CRIADOOOO");
-        //          });
+        
         console.log("Procurando produto por id: " + req.query.produto);
-        produtoModel.findById(req.query.produto, (err, data) => {
-            console.log("Achou: " + data);
-            if (err) {
-                throw err;
+        produtoModel.findById(req.query.produto, (err, produto) => {
+            console.log("Achou: " + produto);
+            if (err) {                
+                res.status(500).json({message: "Produto não encontrado: " + err});
+                return;
             }
+            produto =produto.toObject();
             
-            let id = data.qualificacoes ? data.qualificacoes : mongoose.Types.ObjectId();
+            let id = produto.qualificacoes ? produto.qualificacoes : mongoose.Types.ObjectId();
             console.log(id);
-            console.log(data.qualificacoes);
-            qualificacaoModel.findByIdAndUpdate(id, {$set: {acoes:[]}}, {upsert: true, new: true}, (err, ddd, res) => {
+            console.log(produto.qualificacoes);
+            qualificacaoModel.findByIdAndUpdate(id, {}, {upsert: true, new: true}, (err, qualificacao) => {
                 console.log("ERRR!!!!! " + err);
-                console.log("INSERIU!!!!! " + ddd + res);
+                console.log("INSERIU!!!!! " + qualificacao);
             });
-            // if (!data.qualificacao) {
+            // if (!produto.qualificacao) {
 
             //     qualificacaoModel.create({acoes:[]}).then(qual => {
-            //         data.qualificacao = qual;
+            //         produto.qualificacao = qual;
             //     });
             // } else {
-            //     qualificacaoModel.findById(data.qualificacao);
+            //     qualificacaoModel.findById(produto.qualificacao);
             // }
         });
         
