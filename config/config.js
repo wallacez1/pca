d157db8a38792a68005325723d4c113808dfbdc7
LeandroPA <leandroalencar@unigranrio.br>
2019-06-09 14:00:20 -0300

    descomentado url banco producao

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
 module.exports ={
-//dbStringConexao:`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@pca-ullbn.mongodb.net/test`,
-dbStringConexao:`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@leandro-alencar-1vk7b.mongodb.net/test?retryWrites=true&w=majority`,
+dbStringConexao:`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@pca-ullbn.mongodb.net/test`,
 secret: 'segredoJwt'
 }
