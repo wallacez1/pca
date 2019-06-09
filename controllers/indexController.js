4e4b27ee066b5a57d3e1b761a6743849ceff0027
LeandroPA <leandroalencar@unigranrio.br>
2019-06-09 14:23:33 -0300

    test para tentar encontrar variavel do sistema do heroku

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,6 +4,6 @@ const VERSION = process.env.SOURCE_VERSION || "localhost";
 
 module.exports = {
     index(req, res) {
-        res.status(200).json({api_version: VERSION});
+        res.status(200).json({api_version: process.env});
     }
 };
